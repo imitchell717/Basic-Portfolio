1f42ea1c35689b1d108512ad86d6991d612a7fd8
imitchell717 <56371676+imitchell717@users.noreply.github.com>
2019-12-26 15:15:53 -0500

    adding files

--- java.js
+++ java.js
@@ -1,22 +1,16 @@
-function w3_open() {
-    document.getElementById("main").style.marginLeft = "25%";
- 
-    document.getElementById("mysidebar").style.width = "25%";
+$(".dropdown").aboutme.html({
+    external: true
+});
 
-    document.getElementById("mysidebar").style.display = "block";
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
 
-    document.getElementById("openNav").style.display = "none";
-    $(document).ready(function() {
-        $("body").on('click', '.top', function() {
-            $("nav.menu").toggleClass("menu_show");
-        });
-    });
-}
-
-function w3_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-
-    document.getElementById("mysidebar").style.display = "none";
-
-    document.getElementById("openNav").style.display = "inline-block";
-}
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
